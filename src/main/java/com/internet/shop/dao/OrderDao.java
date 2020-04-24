9eab33ca8d47442c190a70411cd73a3ed02b8b2e
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-04-24 11:43:31 +0300

    refactored DAO and Service layers: introduced GenericDao and GenericService interfaces

--- src/main/java/com/internet/shop/dao/OrderDao.java
+++ src/main/java/com/internet/shop/dao/OrderDao.java
@@ -1,17 +1,6 @@
 package com.internet.shop.dao;
 
 import com.internet.shop.model.Order;
-import java.util.List;
-import java.util.Optional;
 
-public interface OrderDao {
-    Order create(Order order);
-
-    Optional<Order> get(Long id);
-
-    List<Order> getAll();
-
-    Order update(Order order);
-
-    boolean delete(Long id);
+public interface OrderDao extends GenericDao<Order, Long> {
 }
--- src/main/java/com/internet/shop/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/internet/shop/service/impl/ShoppingCartServiceImpl.java
@@ -51,4 +51,29 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     public List<Product> getAllProducts(ShoppingCart shoppingCart) {
         return shoppingCart.getProducts();
     }
+
+    @Override
+    public ShoppingCart create(ShoppingCart shoppingCart) {
+        return shoppingCartDao.create(shoppingCart);
+    }
+
+    @Override
+    public ShoppingCart get(Long id) {
+        return shoppingCartDao.get(id).get();
+    }
+
+    @Override
+    public List<ShoppingCart> getAll() {
+        return shoppingCartDao.getAll();
+    }
+
+    @Override
+    public ShoppingCart update(ShoppingCart shoppingCart) {
+        return shoppingCartDao.update(shoppingCart);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return shoppingCartDao.delete(id);
+    }
 }
