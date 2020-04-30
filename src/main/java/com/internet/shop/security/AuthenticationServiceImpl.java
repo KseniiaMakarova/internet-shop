d522c08830951494a7d0054fab85adf7444a6940
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-04-30 17:44:45 +0300

    changed if clause a bit in AuthenticationServiceImpl - we don't need else block after return statement

--- src/main/java/com/internet/shop/security/AuthenticationServiceImpl.java
+++ src/main/java/com/internet/shop/security/AuthenticationServiceImpl.java
@@ -15,10 +15,9 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     public User login(String login, String password) throws AuthenticationException {
         User user = userService.findByLogin(login).orElseThrow(
                 () -> new AuthenticationException("This login does not exist. Please try again."));
-        if (user.getPassword().equals(password)) {
-            return user;
-        } else {
+        if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("The password is incorrect. Please try again.");
         }
+        return user;
     }
 }
