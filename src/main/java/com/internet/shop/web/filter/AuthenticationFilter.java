29ada23c92f41312a19811baf07f807b012c18b6
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-05-02 16:52:45 +0300

    modified AuthenticationFilter so that it uses excludedServlets list, the list being received from the init-param

--- src/main/java/com/internet/shop/web/filter/AuthenticationFilter.java
+++ src/main/java/com/internet/shop/web/filter/AuthenticationFilter.java
@@ -1,18 +1,31 @@
 package com.internet.shop.web.filter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebFilter("/*")
+@WebFilter(
+        urlPatterns = "/*",
+        initParams = @WebInitParam(name = "excluded", value = "/login,/register"))
 public class AuthenticationFilter implements Filter {
     private static final String USER_ID = "userId";
+    private static List<String> excludedServlets;
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        String excluded = filterConfig.getInitParameter("excluded");
+        excludedServlets = Arrays.asList(excluded.split(","));
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
@@ -20,8 +33,7 @@ public class AuthenticationFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
         String servletPath = req.getServletPath();
-        if (!servletPath.equals("/login")
-                && !servletPath.equals("/register")
+        if (!excludedServlets.contains(servletPath)
                 && req.getSession().getAttribute(USER_ID) == null) {
             resp.sendRedirect(req.getContextPath() + "/login");
         } else {
