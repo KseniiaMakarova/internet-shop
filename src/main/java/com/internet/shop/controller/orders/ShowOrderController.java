63123c4d83d3f535d6b99cc4c96695eba9c7c98e
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-05-10 16:44:52 +0300

    prevented a User from accessing another User's Order via indicating orderId parameter in URL

--- src/main/java/com/internet/shop/controller/orders/ShowOrderController.java
+++ src/main/java/com/internet/shop/controller/orders/ShowOrderController.java
@@ -2,25 +2,47 @@ package com.internet.shop.controller.orders;
 
 import com.internet.shop.lib.Injector;
 import com.internet.shop.model.Order;
+import com.internet.shop.model.Role;
 import com.internet.shop.service.OrderService;
+import com.internet.shop.service.UserService;
+import com.internet.shop.web.filter.AuthorizationFilter;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @WebServlet("/orders/show")
 public class ShowOrderController extends HttpServlet {
+    private static final Logger LOGGER = LogManager.getLogger(AuthorizationFilter.class);
+    private static final String USER_ID = "userId";
     private static final Injector INJECTOR = Injector.getInstance("com.internet.shop");
     private final OrderService orderService =
             (OrderService) INJECTOR.getInstance(OrderService.class);
+    private final UserService userService =
+            (UserService) INJECTOR.getInstance(UserService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        Long viewingUserId = (Long) req.getSession().getAttribute(USER_ID);
         Order order = orderService.get(Long.valueOf(req.getParameter("id")));
-        req.setAttribute("order", order);
-        req.getRequestDispatcher("/views/orders/show.jsp").forward(req, resp);
+        if (isUserAuthorized(viewingUserId, order)) {
+            req.setAttribute("order", order);
+            req.getRequestDispatcher("/views/orders/show.jsp").forward(req, resp);
+        } else {
+            LOGGER.warn("User with ID "
+                    + viewingUserId + " tried to get access to another user's order");
+            req.getRequestDispatcher("/views/accessDenied.jsp").forward(req, resp);
+        }
+    }
+
+    private boolean isUserAuthorized(Long viewingUserId, Order order) {
+        return order.getUserId().equals(viewingUserId)
+                || userService.get(viewingUserId).getRoles().stream()
+                .anyMatch(role -> role.getRoleName().equals(Role.RoleName.ADMIN));
     }
 }
