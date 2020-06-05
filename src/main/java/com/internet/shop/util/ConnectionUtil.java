7c7bfcf6402c1ba0244819657d6be36c1e7f1a44
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-06-05 17:41:36 +0300

    tried to deploy the project to cloud service

--- src/main/java/com/internet/shop/util/ConnectionUtil.java
+++ src/main/java/com/internet/shop/util/ConnectionUtil.java
@@ -5,10 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionUtil {
-    private static final String USER = "root";
-    private static final String PASSWORD = "********";
-    private static final String URL =
-            "jdbc:mysql://localhost:3306/internet_shop?serverTimezone=UTC";
+    private static final String USER = "b380e413ca3b54";
+    private static final String PASSWORD = "f1137036";
+    private static final String URL = "jdbc:mysql://eu-cdbr-west-03.cleardb.net:3306/"
+                    + "heroku_2a0596eefac98c7?serverTimezone=UTC";
 
     static {
         try {
